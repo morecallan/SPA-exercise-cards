91d14026df49ff45493ab1cc655aaa449ca22eb3
Callan Morrison <callan.l.morrison@gmail.com>
2016-03-05 10:49:32 -0600

    completed task 3

--- cards.js
+++ cards.js
@@ -1,11 +1,30 @@
 var textInput = document.getElementById("textInput");
 var addCardButton = document.getElementById("createButton");
 var cardHolder = document.getElementById("cardHolder");
+cardCount = 0;
 
 addCardButton.addEventListener("click", addCardFunction)
 
 function addCardFunction() {
     var cardText = textInput.value
-    var cardString = "<section class='card'> <p>" + cardText + "</p> <button>Delete</button></section>"
-    cardHolder.innerHTML += cardString;
+
+    if (cardText !== "") {
+        var cardString = "<section class='card' id='card" + cardCount + "''> <p>" + cardText + "</p> <button id='delete'>Delete</button></section>"
+        cardHolder.innerHTML += cardString;
+        cardCount ++;
+    } else {
+        alert("Write something, doofus.")
+    }
+}
+
+cardHolder.addEventListener("click", deleteCard);
+
+
+function deleteCard(e){
+    console.log("e", e);
+    if (e.target.id === "delete") {
+        whereAmI = e.target.parentNode;
+        cardHolder.removeChild(whereAmI);
+    }
+    
 }
